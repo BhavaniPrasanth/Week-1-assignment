32cda6fb35073297ec863daf63e6f8022d60b5cf
prasanth <bhavaniprasanth4@gmail.com>
2023-11-01 12:08:27 +0530

    first program

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,16 @@
 */
 
 function isAnagram(str1, str2) {
+  var arr1=str1.split("").sort().join();
+  var arr2=str2.split("").sort().join();
 
+  if(arr1 == arr2){
+    console.log("true");
+  }else{
+    console.log("false")
+  }
 }
+
+isAnagram("aabc", "baac")
 
 module.exports = isAnagram;
